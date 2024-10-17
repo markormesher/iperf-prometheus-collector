bdeb67b04cf8136a35596a0218687c30a78977c3
Mark Ormesher <me@markormesher.co.uk>
2024-10-17 21:58:01 +0100

    actual test running

--- cmd/iperf.go
+++ cmd/iperf.go
@@ -1,19 +1,54 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
 type IperfTcpResult struct {
-	Error string `json:"error"`
-	End   struct {
+	End struct {
 		SumSent struct {
-			Bytes   int `json:"bytes"`
-			Seconds int `json:"seconds"`
+			Bytes   float32 `json:"bytes"`
+			Seconds float32 `json:"seconds"`
 		} `json:"sum_sent"`
 		SumReceived struct {
-			Bytes   int `json:"bytes"`
-			Seconds int `json:"seconds"`
+			Bytes   float32 `json:"bytes"`
+			Seconds float32 `json:"seconds"`
 		} `json:"sum_received"`
 	} `json:"end"`
 }
 
 func runIperfTest(settings *Settings) {
+	for _, target := range settings.TargetList {
+		testsStarted.Value++
 
+		tags := map[string]string{
+			"target": target,
+		}
+
+		cmd := exec.Command("bash", "-c", fmt.Sprintf("iperf3 -c %s --json", target))
+
+		output, err := cmd.Output()
+		if err != nil {
+			l.Error("Failed to run iperf test", "target", target, "error", err, "output", string(output))
+			testsFailed.Value++
+			continue
+		}
+
+		var result IperfTcpResult
+		err = json.NewDecoder(bytes.NewReader(output)).Decode(&result)
+		if err != nil {
+			l.Error("Failed to decode iperf", "target", target, "error", err)
+			testsFailed.Value++
+			continue
+		}
+
+		liveMetrics.Push(Metric{Label: "iperf_sent_bytes", Tags: tags, Value: result.End.SumSent.Bytes})
+		liveMetrics.Push(Metric{Label: "iperf_sent_seconds", Tags: tags, Value: result.End.SumSent.Seconds})
+		liveMetrics.Push(Metric{Label: "iperf_received_bytes", Tags: tags, Value: result.End.SumReceived.Bytes})
+		liveMetrics.Push(Metric{Label: "iperf_received_seconds", Tags: tags, Value: result.End.SumReceived.Seconds})
+		testsFinished.Value++
+	}
 }
--- cmd/metric.go
+++ cmd/metric.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Metric struct {
 	Label string
+	Tags  map[string]string
 	Value float32
 }
 
 func (m *Metric) Format() string {
-	return fmt.Sprintf("%s %f\n", m.Label, m.Value)
+	tags := make([]string, 0)
+	for key, value := range m.Tags {
+		tags = append(tags, fmt.Sprintf("%s=\"%s\"", key, value))
+	}
+
+	tagStr := ""
+	if len(tags) > 0 {
+		tagStr = "{" + strings.Join(tags, ",") + "}"
+	}
+
+	return fmt.Sprintf("%s%s %f\n", m.Label, tagStr, m.Value)
 }
